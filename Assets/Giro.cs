7d41a2b700b62e24b8aa4297c74ee1fd0b9b8281
Damián Cabello <jdamiancabello@gmail.com>
2018-10-01 12:19:50 +0200

    Ejercicio 6 Tema 1 unity terminado y funcional.

---
+++ Assets/Giro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Giro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	System.Random rnd = new System.Random();
+	// Update is called once per frame
+	void Update () {
+		
+		int maximo = 20; 
+		transform.Rotate(rnd.Next(maximo),rnd.Next(maximo),rnd.Next(maximo));
+	}
+}
